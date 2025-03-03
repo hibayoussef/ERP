b94626da74720f6d8f18433453e55e1b9a911279
hyoussef@ridgeco.tech <hyoussef@ridgeco.tech>
2025-03-03 22:19:53 +0300

    [UPDATE] update messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
 // src/App.tsx
-import { useNavigate } from "react-router";
-import { ScrollToTop } from "./components/common/ScrollToTop";
-import { useAxiosInterceptors } from "./hooks/useAxiosInterceptors";
-import RoutesComponent from "./routes/Routes";
-import { useEffect } from "react";
-import { HttpRequestInterceptor } from "./interceptor/http-request.interceptor";
-import { HttpResponseInterceptor } from "./interceptor/http-response.interceptor";
 import { ToastContainer } from "react-toastify";
+import { ScrollToTop } from "./components/common/ScrollToTop";
+import RoutesComponent from "./routes/Routes";
 
 export default function App() {
-  useAxiosInterceptors();
-
-  const navigate = useNavigate();
-  useEffect(() => {
-    HttpRequestInterceptor();
-    HttpResponseInterceptor(navigate);
-  }, [navigate]);
-
   return (
     <>
       <ScrollToTop />
