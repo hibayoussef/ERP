5e5170d82730d05bc99137dcbcf3b397dff9407d
hyoussef@ridgeco.tech <hyoussef@ridgeco.tech>
2025-03-08 07:44:38 +0300

    [UPDATE] update data

--- src/pages/products/Brands/createBrand.tsx
+++ src/pages/products/Brands/createBrand.tsx
@@ -20,14 +20,12 @@ export default function CreateBrand() {
           />
         </div>
 
-        <div className="flex items-start justify-start">
+        <div className="flex justify-end">
           <button
             type="submit"
-            //    disabled={isPending}
-            className="  px-6 py-3 text-sm font-medium disabled:opacity-50 text-white transition rounded-lg  shadow-theme-xs bg-[#575db1] hover:bg-[#474ca1]"
+            className="px-6 py-3 text-sm font-medium disabled:opacity-50 text-white transition rounded-lg shadow-theme-xs bg-[#575db1] hover:bg-[#474ca1]"
           >
-            {" "}
-            {"Submit"}
+            Submit
           </button>
         </div>
       </div>
