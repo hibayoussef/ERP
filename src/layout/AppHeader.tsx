c736e963c8cecda3fd339eb0bc4c21676c9d807a
hibayoussef <hiba.amar.youssef@gmail.com>
2025-03-12 08:32:10 +0300

    [UPDATE] update data

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -83,7 +83,7 @@ const AppHeader: React.FC = () => {
             )}
           </button>
 
-          <Link to="/" className="lg:hidden">
+          {/* <Link to="/" className="lg:hidden">
             <img
               className="dark:hidden"
               src="./images/logo/logo.png"
@@ -94,7 +94,7 @@ const AppHeader: React.FC = () => {
               src="./images/logo/logo-dark.png"
               alt="Logo"
             />
-          </Link>
+          </Link> */}
 
           <button
             onClick={toggleApplicationMenu}
--- src/pages/products/Brands/createBrand.tsx
+++ src/pages/products/Brands/createBrand.tsx
@@ -33,7 +33,7 @@ export default function BrandForm() {
     register,
     handleSubmit,
     setValue,
-    formState: { errors },
+    formState: { errors, isSubmitting },
     control,
   } = useForm<BrandType>({
     resolver: zodResolver(brandSchema),
@@ -128,10 +128,35 @@ export default function BrandForm() {
                 />
               </div>
               <div className="flex justify-end">
-                <button
+              <button
                   type="submit"
-                  className="px-6 py-3 text-sm font-medium disabled:opacity-50 text-white transition rounded-lg shadow-theme-xs bg-[#575db1] hover:bg-[#474ca1]"
+                  className="px-6 py-3 text-sm font-medium disabled:opacity-50 text-white transition rounded-lg shadow-theme-xs bg-[#575db1] hover:bg-[#474ca1] flex items-center gap-2"
+                  disabled={
+                    isSubmitting || addBrand.isPending || updateBrand.isPending
+                  }
                 >
+                  {(addBrand.isPending || updateBrand.isPending) && (
+                    <svg
+                      className="animate-spin h-5 w-5 text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
+                  )}
                   {isUpdate ? "Update" : "Create"}
                 </button>
               </div>
